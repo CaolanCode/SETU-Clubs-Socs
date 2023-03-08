3aaa5a4aae65faa85f2cf0b257a62b428bb5f2fa
CaolanCode <cmacmahon3@gmail.com>
2023-03-08 09:50:36 +0000

    commit sign up contents

--- app.js
+++ app.js
@@ -25,27 +25,58 @@ const signIn = (() => {
   // buttons
   const submitButton = document.createElement('button')
   submitButton.type = 'submit'
-  submitButton.id = 'submit-button'
+  submitButton.id = 'submit-sign-in-btn'
   submitButton.innerText = 'Submit'
   const cancelButton = document.createElement('button')
   cancelButton.type = 'reset'
-  cancelButton.id = 'cancel-button'
+  cancelButton.id = 'cancel-sign-in-btn'
   cancelButton.innerText = 'Cancel'
   inputButtons.appendChild(submitButton)
   inputButtons.appendChild(cancelButton)
 })
 
-const register = (() => {
+const createAccount = (() => {
   clearContainer()
-  // 
+  // student id
+  const username = document.createElement('input')
+  username.placeholder = 'Enter a Username'
+  username.type = 'text'
+  username.name = 'username'
+  username.id = 'username-new'
+  // password
+  const password = document.createElement('input')
+  password.placeholder = 'Password'
+  password.name = 'password'
+  password.type = 'password'
+  password.id = 'password-new'
+  // confirm password
+  const confirmPassword = document.createElement('input')
+  confirmPassword.placeholder = 'Confirm Password'
+  confirmPassword.name = 'password'
+  confirmPassword.type = 'password'
+  confirmPassword.id = 'confirm-password'
+  inputElements.appendChild(username)
+  inputElements.appendChild(password)
+  inputElements.appendChild(confirmPassword)
+  // buttons
+  const submitButton = document.createElement('button')
+  submitButton.type = 'submit'
+  submitButton.id = 'submit-sign-up-btn'
+  submitButton.innerText = 'Submit'
+  const cancelButton = document.createElement('button')
+  cancelButton.type = 'reset'
+  cancelButton.id = 'cancel-sign-up-btn'
+  cancelButton.innerText = 'Cancel'
+  inputButtons.appendChild(submitButton)
+  inputButtons.appendChild(cancelButton)
 })
 
 
-const registerButton = document.getElementById('register')
+const registerButton = document.getElementById('sign-up')
 const signInButton = document.getElementById('sign-in')
 
 signInButton.addEventListener('click', signIn)
-registerButton.addEventListener('click', register)
+registerButton.addEventListener('click', createAccount)
 window.onload = () => signIn()
 
 
