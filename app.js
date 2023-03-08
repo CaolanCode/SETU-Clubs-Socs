10be454490f0029ca12c1879c023506e42cd0b2f
CaolanCode <cmacmahon3@gmail.com>
2023-03-08 09:30:02 +0000

    commit buttons to sign in

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const inputElements = document.querySelector('.input-elements')
+const inputButtons = document.querySelector('.input-buttons')
 
 const clearContainer = (() => {
   inputElements.innerHTML = ''
+  inputButtons.innerHTML = ''
 })
 
 const signIn = (() => {
@@ -20,6 +22,17 @@ const signIn = (() => {
   passwordInput.id = 'pwd-input'
   inputElements.appendChild(usernameInput)
   inputElements.appendChild(passwordInput)
+  // buttons
+  const submitButton = document.createElement('button')
+  submitButton.type = 'submit'
+  submitButton.id = 'submit-button'
+  submitButton.innerText = 'Submit'
+  const cancelButton = document.createElement('button')
+  cancelButton.type = 'reset'
+  cancelButton.id = 'cancel-button'
+  cancelButton.innerText = 'Cancel'
+  inputButtons.appendChild(submitButton)
+  inputButtons.appendChild(cancelButton)
 })
 
 const register = (() => {
